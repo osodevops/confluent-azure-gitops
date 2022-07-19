c181bb9a1daad51f900fac6dd447af48d1a66f42
Sion Smith <sion.smith@gmail.com>
2022-07-19 17:45:05 +0100

    updated known hosts stuff.

--- terraform/flux/kubernetes_flux.tf
+++ terraform/flux/kubernetes_flux.tf
@@ -39,7 +39,7 @@ resource "kubernetes_secret" "common" {
   data = {
     identity       = base64decode(data.azurerm_key_vault_secret.confluent_ssh_key.value)
     "identity.pub" = data.azurerm_ssh_public_key.confluent_public_key.public_key
-    known_hosts    = "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
+    known_hosts = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
   }
 }
 
